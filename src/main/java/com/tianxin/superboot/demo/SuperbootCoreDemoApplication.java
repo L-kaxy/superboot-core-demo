4bc1b792f53386b825102ef90298840c683ad148
DESKTOP-B3C5HOJ\L-kaxy <770619071@qq.com>
2018-06-14 16:58:16 +0800

    demo 项目初始化

---
+++ src/main/java/com/tianxin/superboot/demo/SuperbootCoreDemoApplication.java
@@ -0,0 +1,12 @@
+package com.tianxin.superboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperbootCoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperbootCoreDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tianxin/superboot/demo/SuperbootCoreDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tianxin.superboot.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperbootCoreDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
